e981fadc610cf8ddfaf6f6ea600f240101dea793
kokinz <nes-t@mail.ru>
2021-06-21 22:35:11 +0300

    done

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {filmsCount, filmName, filmGenre, filmYear} = props;
+
+  return (
+    <MainPage filmsCount={filmsCount} filmName={filmName} filmGenre={filmGenre} filmYear={filmYear} />
+  );
 }
+
+App.propTypes = {
+  filmsCount: PropTypes.number.isRequired,
+  filmName: PropTypes.string.isRequired,
+  filmGenre: PropTypes.string.isRequired,
+  filmYear: PropTypes.string.isRequired,
+};
 
 export default App;
