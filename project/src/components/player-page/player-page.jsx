b327f8ca9f4deb53ffd1d2d25f15b0aa7e10c94d
kokinz <nes-t@mail.ru>
2021-07-27 19:32:47 +0300

    fullScreen fix

--- project/src/components/player-page/player-page.jsx
+++ project/src/components/player-page/player-page.jsx
@@ -7,8 +7,6 @@ import PropTypes from 'prop-types';
 import filmProp from '../film-page/film.prop';
 import {APIRoute, ROUNDING_INTEGER} from '../../const.js';
 
-const ESCAPE_BUTTON = 'Escape';
-
 function PlayerPage({films}) {
   const filmId = parseInt(useParams().id, ROUNDING_INTEGER);
   const film = films.find((movie) => (movie.id === filmId));
@@ -20,7 +18,6 @@ function PlayerPage({films}) {
 
   const [playerState, setPlayerState] = useState({
     isPlaying: false,
-    isFullScreen: false,
     elapsedTime: '',
   });
 
@@ -44,59 +41,45 @@ function PlayerPage({films}) {
     }
   };
 
-  const handleEscapeKeydown = (evt) => {
-    if (evt.key === ESCAPE_BUTTON) {
-      evt.preventDefault();
-
-      setPlayerState((prevState) => ({...prevState, isFullScreen: false}));
-
-      document.removeEventListener('keydown', handleEscapeKeydown);
-    }
-  };
-
   const handleFullScreenClick = (evt) => {
     evt.preventDefault();
 
-    setPlayerState((prevState) => ({...prevState, isFullScreen: true}));
-
-    document.addEventListener('keydown', handleEscapeKeydown);
+    player.current.requestFullscreen();
   };
 
   return (
     <div className="player">
       <video ref={player} src={film.videoLink} className="player__video" poster={film.posterImage} />
-      {playerState.isFullScreen ? '' :
-        <>
-          <button type="button" className="player__exit" onClick={() => history.push(`${APIRoute.FILMS}/${filmId}`)}>Exit</button>
-          <div className="player__controls">
-            <div className="player__controls-row">
-              <div className="player__time">
-                <progress className="player__progress" value={30} max={100} />
-                <div className="player__toggler" style={{left: `${Math.floor((elapsedTime/(duration*60)*100))}`}}>Toggler</div>
-              </div>
-              <div className="player__time-value">
-                <Moment unix format="-HH:mm:ss">
-                  {remainingTime}
-                </Moment>
-              </div>
-            </div>
-            <div className="player__controls-row">
-              <button ref={playButton} type="button" className="player__play" onClick={handlePlayClick}>
-                <svg viewBox="0 0 19 19" width={19} height={19}>
-                  <use xlinkHref={playerState.isPlaying ? '#pause' : '#play-s'}/>
-                </svg>
-                <span>Play</span>
-              </button>
-              <div className="player__name">{film.name}</div>
-              <button ref={fullScreenButton} type="button" className="player__full-screen" onClick={handleFullScreenClick}>
-                <svg viewBox="0 0 27 27" width={27} height={27}>
-                  <use xlinkHref="#full-screen" />
-                </svg>
-                <span>Full screen</span>
-              </button>
-            </div>
+
+      <button type="button" className="player__exit" onClick={() => history.push(`${APIRoute.FILMS}/${filmId}`)}>Exit</button>
+      <div className="player__controls">
+        <div className="player__controls-row">
+          <div className="player__time">
+            <progress className="player__progress" value={30} max={100} />
+            <div className="player__toggler" style={{left: `${Math.floor((elapsedTime/(duration*60)*100))}`}}>Toggler</div>
           </div>
-        </>}
+          <div className="player__time-value">
+            <Moment unix format="-HH:mm:ss">
+              {remainingTime}
+            </Moment>
+          </div>
+        </div>
+        <div className="player__controls-row">
+          <button ref={playButton} type="button" className="player__play" onClick={handlePlayClick}>
+            <svg viewBox="0 0 19 19" width={19} height={19}>
+              <use xlinkHref={playerState.isPlaying ? '#pause' : '#play-s'}/>
+            </svg>
+            <span>Play</span>
+          </button>
+          <div className="player__name">{film.name}</div>
+          <button ref={fullScreenButton} type="button" className="player__full-screen" onClick={handleFullScreenClick}>
+            <svg viewBox="0 0 27 27" width={27} height={27}>
+              <use xlinkHref="#full-screen" />
+            </svg>
+            <span>Full screen</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
