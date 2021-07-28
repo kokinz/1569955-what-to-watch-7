55ccfc031e77bcc2096ee3fdd17d1511eb38d386
kokinz <nes-t@mail.ru>
2021-07-28 14:07:23 +0300

    fix addReview Redirect

--- project/src/store/middlewares/redirect.js
+++ project/src/store/middlewares/redirect.js
@@ -3,7 +3,8 @@ import {ActionType} from '../action.js';
 
 const redirect = (_store) => (next) => (action) => {
   if (action.type === ActionType.REDIRECT_TO_ROUTE) {
-    browserHistory.push(action.payload);
+    browserHistory.push(`/1569955-what-to-watch-7/#${action.payload}`);
+    window.location.reload();
   }
 
   return next(action);
